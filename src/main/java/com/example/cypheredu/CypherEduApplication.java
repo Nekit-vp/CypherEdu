152d785a60194e76aab843f032024045fdad3b0b
nikita <lobanov@technocratos.com>
2022-04-13 20:38:22 +0300

    init commit

---
+++ src/main/java/com/example/cypheredu/CypherEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.cypheredu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CypherEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CypherEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cypheredu/CypherEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cypheredu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypherEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
